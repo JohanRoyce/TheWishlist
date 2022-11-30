9a4c199c47c3adc9acf13ac9adac0ebcfe5ac992
JohanRoyce <johanroyce1@gmail.com>
2022-11-29 23:18:09 -0500

    Hero page ready

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Home' });
+});
+
+
+/* GET home page. */
+router.get('/gifts', function(req, res, next) {
   res.render('index', { title: 'Gift Wishlist' });
+});
+
+
+/* GET home page. */
+router.get('/home', function(req, res, next) {
+  res.render('index', { title: 'Home' });
 });
 
 module.exports = router;
